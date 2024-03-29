8f7943b02edced7d0d365be0910cd37230f38ba8
Sashank <sashank.k04@gmail.com>
2024-03-29 19:31:26 -0400

    updated_db

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from joblib import load
 import mysql.connector
 import os
 import json
+import logging
 
 app = Flask(__name__)
 
@@ -11,17 +12,55 @@ CORS(app)
 # Load the trained RandomForest model
 model = load('RandomForest_best_model.pkl')
 
+logger = logging.getLogger(__name__)
+
+
+###Check the Databse Configuration
+
 # Set up MySQL connection parameters
 db_config = {
-    "host": os.getenv("DB_HOST"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME")
+    "host": os.environ.get("DB_HOST"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_NAME")
 }
+
+@app.route('/<patient_id>', methods=['GET'])
+def home_page(patient_id):
+    return f"Hello World {patient_id}"
+    
+@app.route('/test_db_connection')
+def test_db_connection():
+    cursor = None
+    connection = None
+
+    try:
+        # Connect to the PostgreSQL database
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        # Perform a simple query to check the database connection
+        cursor.execute("SELECT count(*) FROM atrial_fibrillation;")
+        count = cursor.fetchone()[0]
+
+        return jsonify({'message': f'Database connection successful. Found {count} records in the table'}), 200
+
+    except mysql.connector.Error as e:
+        return jsonify({'error': f'Database error: {e}'}), 500
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
 
 @app.route('/predict/patient/<patient_id>', methods=['GET'])
 def predict_by_patient_id(patient_id):
+    cursor = None
+
     try:
+        logger.debug('Start of prediction process')
         # Connect to the MySQL database
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
@@ -29,6 +68,7 @@ def predict_by_patient_id(patient_id):
         # Fetch atrial fibrillation data from the atrial_fibrillation table based on patient_id
         cursor.execute(f"SELECT Age, Gender, Channel_1, Channel_2, Channel_3, Channel_4, Channel_5, Channel_6, Channel_7, Channel_8, Channel_9, Channel_10, Channel_11, Channel_12 FROM atrial_fibrillation WHERE patient_id = {patient_id}")
         patient_data = cursor.fetchone()
+        logging.debug('Cursor ',patient_data)
 
         if not patient_data:
             return jsonify({'error': 'Patient data not found'}), 404
@@ -42,12 +82,18 @@ def predict_by_patient_id(patient_id):
 
         # Extract features from each channel's JSON data
         for channel_json in channels:
-            channel_data = json.loads(channel_json)
+            # New approach, check type first:
+            if isinstance(channel_json, str):
+                channel_data = json.loads(channel_json)  # Decode JSON string to dict
+            else:
+                channel_data = channel_json  # Use directly if already a dict
             features.extend([channel_data['SDNN'][0], channel_data['RMSSD'][0], channel_data['PNN50'][0], channel_data['Mean_RR'][0]])
 
         # Prediction
+        logger.debug('Stating pred')
         prediction = model.predict([features])[0]
         # Assuming your model's output maps directly to these labels
+        logger.debug('Finish pred')
         prediction_label = {
             0: 'Atrial Fibrillation',
             1: 'Sinus Rhythm',
@@ -64,6 +110,7 @@ def predict_by_patient_id(patient_id):
         })
     
     except mysql.connector.Error as err:
+        logger.debug('Found an error')
         return jsonify({'error': f'Database error: {err}'}), 500
     
     finally:
